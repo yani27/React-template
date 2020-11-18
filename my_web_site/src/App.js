03759ec4bfb32a0a1138be03cce15caee8a0fb02
yani27 <yanilacamora@gmail.com>
2020-11-18 16:59:24 +0100

    1.1 vers

--- my_web_site/src/App.js
+++ my_web_site/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
     <Router>
-      <navbar/>
+      <Navbar/>
       <Switch>
         <Route path='/'exact />
       </Switch>
